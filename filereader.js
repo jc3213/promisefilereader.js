dea2dd40b0c0d635253c710cfbd35c55b7cf039a
jc3213 <jc3213@users.noreply.github.com>
2025-04-12 20:34:40 +0800

    Update filereader.js

--- filereader.js
+++ filereader.js
@@ -1,28 +1,29 @@
 class PromiseFileReader {
-    reader (file, method) {
+    version = '0.1';
+    text (file) {
+        return PromiseFileReader.read(file, 'readAsText');
+    }
+    dataURL (file) {
+        return PromiseFileReader.read(file, 'readAsDataURL');
+    }
+    arrayBuffer (file) {
+        return PromiseFileReader.read(file, 'readAsArrayBuffer');
+    }
+    binaryString (file) {
+        return PromiseFileReader.read(file, 'readAsBinaryString');
+    }
+    json (file) {
+        return PromiseFileReader.read(file, 'readAsText').then((str) => JSON.parse(str));
+    }
+    base64 (file) {
+        return PromiseFileReader.read(file, 'readAsDataURL').then((str) => str.slice(str.indexOf(',') + 1));
+    }
+    static read (file, method) {
         return new Promise((resolve, reject) => {
             var reader = new FileReader();
             reader.onload = (event) => resolve(event.target.result);
             reader.onerror = reject;
             reader[method](file);
         });
-    }
-    text (file) {
-        return this.reader(file, 'readAsText');
-    }
-    dataURL (file) {
-        return this.reader(file, 'readAsDataURL');
-    }
-    arrayBuffer (file) {
-        return this.reader(file, 'readAsArrayBuffer');
-    }
-    binaryString (file) {
-        return this.reader(file, 'readAsBinaryString');
-    }
-    json (file) {
-        return this.reader(file, 'readAsText').then((str) => JSON.parse(str));
-    }
-    base64 (file) {
-        return this.reader(file, 'readAsDataURL').then((str) => str.slice(str.indexOf(',') + 1));
     }
 }
